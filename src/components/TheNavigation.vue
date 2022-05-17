67f67d38f6d93d10c03de95a5f0ece05dca66ada
Yosua77 <yosuayuwono@gmail.com>
2022-05-17 21:14:44 +0700

    Init + Desktop Navigation

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<script>
+export default {
+  created() {
+    console.log("Navigation Created");
+  },
+};
+</script>
+
+<template>
+  <nav class="navigation-container">
+    <div class="left-side">
+      <RouterLink to="/shop" class="navigation-link">SHOP</RouterLink>
+      <RouterLink to="/about" class="navigation-link">ABOUT</RouterLink>
+      <RouterLink to="/collection" class="navigation-link"
+        >COLLECTION</RouterLink
+      >
+    </div>
+    <RouterLink to="/" class="logo h3"> HYPE </RouterLink>
+    <div class="right-side">
+      <RouterLink to="/search" class="navigation-link">SEARCH</RouterLink>
+      <p class="navigation-link">CART <span>(0)</span></p>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.navigation-container {
+  width: 100%;
+  max-width: calc(100% - 6rem);
+  padding: 0 2rem;
+  background-color: var(--color-background);
+  display: flex;
+  align-items: center;
+  border: solid 2px var(--color-border-dark);
+  border-top: none;
+}
+
+.navigation-container .left-side {
+  flex: 1;
+  display: flex;
+  gap: 2rem;
+  align-items: center;
+}
+
+.navigation-container .right-side {
+  flex: 1;
+  display: flex;
+  gap: 2rem;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+.logo {
+  font-weight: 900;
+  line-height: normal;
+}
+
+.navigation-link {
+  letter-spacing: 0.2rem;
+  cursor: pointer;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+console.log("TESTING");
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h5>Hello</h5>
   </main>
 </template>
+
+<style scoped>
+main {
+  width: 100%;
+  max-width: calc(100% - 6rem);
+  aspect-ratio: 16/9;
+  background-color: var(--color-container-dark);
+  background-image: url("/video.gif");
+  background-repeat: no-repeat;
+  background-size: contain;
+}
+</style>
