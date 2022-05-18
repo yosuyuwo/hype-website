efaecbc14978677b504882a2f93e1c2d21b71e89
marvelchbp <57288773+marvelchbp@users.noreply.github.com>
2022-05-18 16:04:38 +0700

    Create DataStorage.vue

---
+++ src/components/DataStorage.vue
@@ -0,0 +1,67 @@
+<script>
+export default {
+  mounted() {
+    window.addEventListener("scroll", this.onScroll);
+  },
+  data() {
+    return {
+      brands: [
+        {
+          name: "adidas",
+          video: {
+            title: "adi",
+          },
+          products: [
+            {
+              title: "titel",
+              subtitle: "subtitel",
+              type: "shoes",
+              gender: "men",
+              size: ["8"],
+              colours: ["#FFFFF"],
+              price: 100000,
+              stock: 10,
+            },
+          ],
+        },
+        {
+          name: "nike",
+          video: {
+            title: "adi",
+          },
+          products: [
+            {
+              title: "titel",
+              subtitle: "subtitel",
+              type: "shoes",
+              gender: "men",
+              size: ["8"],
+              colours: ["#FFFFF"],
+              price: 100000,
+              stock: 10,
+            },
+          ],
+        },
+        {
+          name: "jordan",
+          video: {
+            title: "adi",
+          },
+          products: [
+            {
+              title: "titel",
+              subtitle: "subtitel",
+              type: "shoes",
+              gender: "men",
+              size: ["8"],
+              colours: ["#FFFFF"],
+              price: 100000,
+              stock: 10,
+            },
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
